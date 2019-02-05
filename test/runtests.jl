c938fa654abc163ff7a25809a7baf0325e7be03e
MaximeBouton <maxim.bouton@gmail.com>
2019-02-05 11:44:47 -0800

    more robust tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,6 @@ using POMDPSimulators
 using Flux
 using DeepQLearning
 using DiscreteValueIteration
-Random.seed!(1)
 
 test_env_path = joinpath(dirname(pathof(DeepQLearning)),"..", "test", "test_env.jl")
 include(test_env_path)
@@ -23,12 +22,14 @@ function evaluate(mdp, policy, rng, n_ep=100, max_steps=100)
 end
 
 @testset begin "Dummy Corrections"
+    # set random seed for test block
+    Random.seed!(1)
     rng = MersenneTwister(1)
     mdp = TestMDP((5,5), 4, 6)
     model = Chain(x->flattenbatch(x), Dense(100, 8, tanh), Dense(8, 4))
-    dqn_solver = DeepQLearningSolver(qnetwork = model, max_steps=10000, learning_rate=0.005, 
+    dqn_solver = DeepQLearningSolver(qnetwork = model, max_steps=20000, learning_rate=0.005, 
                                     eval_freq=2000,num_ep_eval=100,
-                                    log_freq = 500,
+                                    log_freq = 1000,
                                     double_q = false, dueling=true, prioritized_replay=false,
                                     rng=rng)
     solver = DeepCorrectionSolver(dqn = dqn_solver)
@@ -38,6 +39,8 @@ end
 end
 
 @testset begin "Perfect Qlo"
+    # set random seed for test block
+    Random.seed!(1)
     rng = MersenneTwister(1)
     # value table 
     mdp = SimpleGridWorld()
@@ -48,7 +51,8 @@ end
         return vi_pol.qmat[si, :]
     end
     model = Chain(x->flattenbatch(x), Dense(2, 32, relu), Dense(32, n_actions(mdp)))
-    dqn_solver = DeepQLearningSolver(qnetwork = model, prioritized_replay=true, max_steps=40_000, learning_rate=0.01,log_freq=500,
+    dqn_solver = DeepQLearningSolver(qnetwork = model, prioritized_replay=true, max_steps=40_000, learning_rate=0.01,
+                                     log_freq=1000, eval_freq = 2000,
                                      eps_fraction = 0.1, eps_end = 0.01,
                                      double_q=true, dueling=true, rng=rng)
     solver = DeepCorrectionSolver(dqn = dqn_solver, 
